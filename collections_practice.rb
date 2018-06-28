726be43a3b777f4de2cd4b58b369eb25f28d4333
jmoypace <jmoypace@gmail.com>
2018-06-28 00:01:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,20 +13,34 @@ array.sort do |a, b|
 end
 end
 def swap_elements(array)
-
+  array[1], array[2] = array[2], array[1]
+    array
 end
 def reverse_array(array)
 array.reverse
 end
 def kesha_maker(array)
-
+array.each do|word|word[2]="$"
 end
+end
+<<<<<<< HEAD
 def find_a(array)
-
+  array.find_all do |word|
+  word[0] == "a"
 end
 def sum_array(array)
-
+  sum = 0
+    array.each do |num|
+      sum+=num
+    end
+    sum
 end
 def add_s(array)
-
+  array.collect do |word|
+      if array[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
 end
